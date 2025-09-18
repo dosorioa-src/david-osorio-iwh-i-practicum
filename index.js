40b7af0d64c3e5f8b607b5ff83244a195e377e64
david <dosorioa@autonoma.edu.pe>
2025-09-18 15:16:00 -0500

    List pets in home page

--- index.js
+++ index.js
@@ -8,11 +8,43 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
-const PRIVATE_APP_ACCESS = '';
+require('dotenv').config();
+const PRIVATE_APP_ACCESS = process.env.PRIVATE_APP_ACCESS;
+
+if (!PRIVATE_APP_ACCESS) {
+    console.warn('Warning: HUBSPOT_TOKEN no está configurado.');
+}
+
+const hubspot = axios.create({
+    baseURL: 'https://api.hubapi.com',
+    headers: {
+        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+        'Content-Type': 'application/json'
+    }
+});
 
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
 
-// * Code for Route 1 goes here
+
+function propertiesQuery(props) {
+    return props && props.length ? `properties=${props.join('&properties=')}` : '';
+}
+
+app.get('/', async (req, res) => {
+    try {
+        const defaultProperties = ['nombre', 'sexo', 'edad'];
+
+        const propsQuery = propertiesQuery(defaultProperties);
+
+        const url = `/crm/v3/objects/p_pets?limit=100&${propsQuery}`;
+        const resp = await hubspot.get(url);
+
+        res.render('homepage', { title: 'homepage', pets: resp.data.results || [] });
+    } catch (err) {
+        console.error('Error fetching pets:', err?.response?.data || err.message);
+        res.status(500).send('Error fetching pets. Revisa la consola del servidor.');
+    }
+});
 
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 
@@ -22,49 +54,6 @@ const PRIVATE_APP_ACCESS = '';
 
 // * Code for Route 3 goes here
 
-/** 
-* * This is sample code to give you a reference for how you should structure your calls. 
-
-* * App.get sample
-app.get('/contacts', async (req, res) => {
-    const contacts = 'https://api.hubspot.com/crm/v3/objects/contacts';
-    const headers = {
-        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
-        'Content-Type': 'application/json'
-    }
-    try {
-        const resp = await axios.get(contacts, { headers });
-        const data = resp.data.results;
-        res.render('contacts', { title: 'Contacts | HubSpot APIs', data });      
-    } catch (error) {
-        console.error(error);
-    }
-});
-
-* * App.post sample
-app.post('/update', async (req, res) => {
-    const update = {
-        properties: {
-            "favorite_book": req.body.newVal
-        }
-    }
-
-    const email = req.query.email;
-    const updateContact = `https://api.hubapi.com/crm/v3/objects/contacts/${email}?idProperty=email`;
-    const headers = {
-        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
-        'Content-Type': 'application/json'
-    };
-
-    try { 
-        await axios.patch(updateContact, update, { headers } );
-        res.redirect('back');
-    } catch(err) {
-        console.error(err);
-    }
-
-});
-*/
 
 
 // * Localhost
